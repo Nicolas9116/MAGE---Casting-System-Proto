c53ece1256bbf61bc38e13803d1f36c7431afd63
Nicolas9116 <106292589+Nicolas9116@users.noreply.github.com>
2024-06-06 12:20:12 +0930

    Update ComboLight.cpp

--- Casting System Proto/ComboLight.cpp
+++ Casting System Proto/ComboLight.cpp
@@ -1,9 +1,29 @@
 #include "ComboLight.hpp"
 
 
-	ComboLight::ComboLight(sf::Sprite& litSprite, sf::Sprite& unlitSprite)
-		: litSprite(litSprite), unlitSprite(unlitSprite), currentLight(unlitSprite)
+	ComboLight::ComboLight(sf::Sprite& litSprite, sf::Sprite& unlitSprite, sf::Texture& litTex, sf::Texture& unLitTex)
 	{
+		unLitSprite.setTexture(unLitTex);
+		LitSprite.setTexture(LitTex);
+		
+		if(inputValue == 1)
+		{
+			sf::Sprite currentSprite.setRotation(0);	
+		}
+				if(inputValue == 2)
+		{
+			sf::Sprite currentSprite.setRotation(180);	
+		}
+				if(inputValue == 3)
+		{
+			sf::Sprite currentSprite.setRotation(270);	
+		}
+				if(inputValue == 4)
+		{
+			sf::Sprite currentSprite.setRotation(90);	
+		}
+
+		currentLight& = unLitSprite;
 	}
 
 	void ComboLight::LightOn()
